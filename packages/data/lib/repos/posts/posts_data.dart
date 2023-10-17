67e6edabad9467f8af9136cae45ebb6eb8c0597c
Morgan <mrgnhnt96@gmail.com>
2023-10-17 14:53:43 -0500

    feat: add comments

--- packages/data/lib/repos/posts/posts_data.dart
+++ packages/data/lib/repos/posts/posts_data.dart
@@ -148,5 +148,153 @@ const posts = [
       'username': 'nmcilhagga3',
       'image': 'https://source.unsplash.com/random/300×300?profile',
     },
+  },
+  {
+    'id': '5DDF65A7-84B2-4925-9E90-41DBEA9DA20E',
+    'parent': 'B5630899-4C4F-4F10-9A9F-EAFEBA728B58',
+    'posted': '2022-05-20T00:06:14Z',
+    'content':
+        'pede lobortis ligula sit amet eleifend pede libero quis orci nullam molestie nibh in lectus pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate luctus cum sociis natoque',
+    'images': [],
+    'likes': 3362,
+    'comments': 0,
+    'owner': {
+      'id': 1,
+      'image': 'https://source.unsplash.com/random/300×300?profile',
+      'username': 'tberrick0',
+    },
+  },
+  {
+    'id': 'A398C579-5EDF-46B2-943B-BD0AB675FF07',
+    'parent': 'F0706570-5568-42F1-B74A-8DBFC79DEE90',
+    'posted': '2022-12-02T18:15:56Z',
+    'content':
+        'semper rutrum nulla nunc purus phasellus in felis donec semper sapien a libero nam',
+    'images': [],
+    'likes': 8084,
+    'comments': 0,
+    'owner': {
+      'id': 8,
+      'image': 'https://source.unsplash.com/random/300×300?profile',
+      'username': 'trickard7',
+    },
+  },
+  {
+    'id': 'CAF97B47-CCC7-4201-A971-F0FFD51478DD',
+    'parent': '47840546-22A0-49E2-8E53-0EBA3AFB9D5D',
+    'posted': '2022-08-18T18:25:00Z',
+    'content':
+        'metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet diam in magna bibendum imperdiet nullam orci pede venenatis non sodales sed tincidunt eu felis fusce',
+    'images': [],
+    'likes': 9501,
+    'comments': 0,
+    'owner': {
+      'id': 4,
+      'image': 'https://source.unsplash.com/random/300×300?profile',
+      'username': 'nmcilhagga3',
+    },
+  },
+  {
+    'id': '1A993127-352C-4479-A2C8-C5055B7CD70B',
+    'parent': 'E5912EB2-6DBA-4979-AB99-0BD74AB85233',
+    'posted': '2023-05-27T22:36:24Z',
+    'content': 'donec odio justo sollicitudin ut suscipit a feugiat et eros',
+    'images': [],
+    'likes': 3946,
+    'comments': 0,
+    'owner': {
+      'id': 7,
+      'image': 'https://source.unsplash.com/random/300×300?profile',
+      'username': 'asongest6',
+    },
+  },
+  {
+    'id': 'F767D58A-17D0-497F-BD32-43A5D797D6AF',
+    'parent': '6FDC8316-443B-468A-BCB5-704B94210F3C',
+    'posted': '2022-04-24T08:03:38Z',
+    'content':
+        'sed vel enim sit amet nunc viverra dapibus nulla suscipit ligula in lacus curabitur at ipsum ac tellus semper',
+    'images': [],
+    'likes': 97,
+    'comments': 0,
+    'owner': {
+      'id': 8,
+      'image': 'https://source.unsplash.com/random/300×300?profile',
+      'username': 'trickard7',
+    },
+  },
+  {
+    'id': '1E72EE28-1D3C-4F0C-8B6A-2EE7D43EBA5E',
+    'parent': '5FEC49E0-498E-4F07-9F41-A46909F98FE8',
+    'posted': '2022-03-17T14:33:47Z',
+    'content':
+        'nunc donec quis orci eget orci vehicula condimentum curabitur in libero ut massa volutpat convallis morbi odio odio elementum eu interdum eu tincidunt in leo maecenas pulvinar lobortis',
+    'images': [],
+    'likes': 3954,
+    'comments': 0,
+    'owner': {
+      'id': 10,
+      'image': 'https://source.unsplash.com/random/300×300?profile',
+      'username': 'vsmalecombe9',
+    },
+  },
+  {
+    'id': '5B20B692-F41C-4E2C-88AD-25EC13796F00',
+    'parent': '5FEC49E0-498E-4F07-9F41-A46909F98FE8',
+    'posted': '2023-06-04T04:19:36Z',
+    'content':
+        'aliquam erat volutpat in congue etiam justo etiam pretium iaculis justo in hac habitasse platea dictumst etiam faucibus cursus urna ut tellus nulla ut',
+    'images': [],
+    'likes': 7352,
+    'comments': 0,
+    'owner': {
+      'id': 5,
+      'image': 'https://source.unsplash.com/random/300×300?profile',
+      'username': '',
+    },
+  },
+  {
+    'id': '585DCAD4-1C05-49AC-9F0D-B31664376A72',
+    'parent': '6FDC8316-443B-468A-BCB5-704B94210F3C',
+    'posted': '2023-03-13T22:34:32Z',
+    'content':
+        'proin risus praesent lectus vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae duis faucibus accumsan',
+    'images': [],
+    'likes': 7911,
+    'comments': 0,
+    'owner': {
+      'id': 9,
+      'image': 'https://source.unsplash.com/random/300×300?profile',
+      'username': 'rpinwell8',
+    },
+  },
+  {
+    'id': '92F0AE12-4B0C-4C64-B108-5848AD51DC1B',
+    'parent': 'B5630899-4C4F-4F10-9A9F-EAFEBA728B58',
+    'posted': '2023-03-19T03:32:49Z',
+    'content':
+        'eu tincidunt in leo maecenas pulvinar lobortis est phasellus sit amet erat nulla tempus vivamus in felis eu sapien',
+    'images': [],
+    'likes': 3815,
+    'comments': 0,
+    'owner': {
+      'id': 3,
+      'image': 'https://source.unsplash.com/random/300×300?profile',
+      'username': 'pllorente2',
+    },
+  },
+  {
+    'id': '576C947E-963F-49F0-A77C-3FE64ED84533',
+    'parent': '869F462A-1884-4D83-A51D-7438E0F8F142',
+    'posted': '2023-01-04T05:04:46Z',
+    'content': 'nulla nunc purus phasellus in felis donec semper sapien a',
+    'images': [],
+    'likes': 2015,
+    'comments': 0,
+    'owner': {
+      'id': 9,
+      'image': 'https://source.unsplash.com/random/300×300?profile',
+      'username': 'rpinwell8',
+    },
   }
 ];
